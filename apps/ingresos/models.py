3cd70233dd0741b851809aea1f53826f97a45bc6
aribt82 <142463431+aribt82@users.noreply.github.com>
2025-06-03 20:02:29 -0300

    Update models.py

--- apps/ingresos/models.py
+++ apps/ingresos/models.py
@@ -2,12 +2,12 @@
 from django.db import models
 
 class Remito(models.Model):
-    # id = models.IntegerField(3)
+    id = models.AutoField(primary_key=True)
     numero_remito = models.CharField(max_length=20)
-    numero_viaje = models.IntegerField(5)
+    numero_viaje = models.IntegerField()
     detalle_transporte = models.CharField(max_length=30)
     deposito_id = models.ForeignKey('depositos.Deposito', on_delete=models.PROTECT)
-    fecha_ingreso = models.DateTimeField
+    fecha_ingreso = models.DateTimeField()
     usuario_id = models.ForeignKey('usuarios.Usuario', on_delete=models.PROTECT)
     aprobado = models.BooleanField(False)
     
@@ -16,17 +16,20 @@ class Remito(models.Model):
 
 
 class RemitoProducto(models.Model):
-    # id = models.IntegerField(3)
-    remito_id = models.ForeignKey('ingresos.Remito', on_delete=models.PROTECT)
-    producto_id = models.ForeignKey('ingresos.Producto', on_delete=models.PROTECT)
-    cantidad = models.IntegerField(4)
-    actualizado = models.DateTimeField
+    id = models.AutoField(primary_key=True)
+    remito_id = models.ForeignKey('Remito', on_delete=models.PROTECT)
+    producto_id = models.ForeignKey('Producto', on_delete=models.PROTECT)
+    cantidad = models.IntegerField()
+    actualizado = models.DateTimeField()
+    
+    def __str__(self):
+        return f"{self.producto_id} {self.cantidad} {self.remito_id}"
 
 
 class Producto(models.Model):
-    # id = models.IntegerField(3)
+    id = models.AutoField(primary_key=True)
     marca = models.CharField(30)
-    modelo = models.CharField(50)
+    modelo = models.CharField(50, unique=True)
 
     def __str__(self):
         return f"{self.marca} {self.modelo}"
